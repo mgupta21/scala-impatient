11dc7df793728fac7ba57424b39dc6af6e79b94b
mgupta <mayank.gupta@here.com>
2017-02-01 01:27:54 -0600

    Added lab 2

---
+++ src/main/scala/Lab2.scala
@@ -0,0 +1,44 @@
+/**
+  * Created by mgupta on 2/1/17.
+  */
+object Lab2 extends App {
+
+  def vowels(s: String) = {
+    var result = ""
+    for (ch <- s) if (isVowel(ch)) result += ch
+    result
+  }
+
+  def vowels2(s: String) = for (ch <- s if isVowel(ch)) yield ch
+
+  def vowels3(s: String): String = {
+    if (s.length == 0) ""
+    else {
+      if (isVowel(s(0))) vowels3(s) else vowels(s.tail)
+    }
+  }
+
+  def vowels4(s: String) = {
+    var i = 0
+    var result = ""
+    while (i < s.length) {
+      if (isVowel(s(i))) result += s(i)
+      i += 1
+    }
+    result
+  }
+
+  def isVowel(ch: Char) = "aeiou".contains(ch)
+
+  println(vowels("Nicaragua"))
+  println(vowels2("Nicaragua"))
+  println(vowels3("Nicaragua"))
+  println(vowels4("Nicaragua"))
+
+  def vowels2(s: String, vowelsChars: String, ignoreCase: Boolean = true) = for (ch <- (if (ignoreCase) s.toLowerCase() else s) if isVowel(ch, vowelsChars)) yield ch
+
+  def isVowel(ch: Char, vowelsChars: String) = vowelsChars.contains(ch)
+
+  println(vowels2("Übeltätergehör", "aeiouüäö"))
+
+}
--- src/main/scala/Sheet.scala
+++ src/main/scala/Sheet.scala
@@ -11,27 +11,24 @@ object Sheet extends App {
   val answer = 8 + 5
   // answer = 76  // Cannot reassign a val
   answer
-
-  // Var is mutable
-  var response = 54
+  val x: BigInt = 12345678
   // Can reassign var
   response = 78
-
-  var greeting: String = null
+  val result = 1.+(10)
   greeting = "Hello"
-
-  var count: Int = 2
+  // In Scala, an expression has a value
+  val exp = if (z > 0) 1 else -1 // Like x > 0 ? 1 : -1 in java
 
   // Everything is a object. Creating Range
   1.to(10)
   1 to 10
 
   "Hello".intersect("World") // Yields "lo"
-
-  val x: BigInt = 12345678
+  // Type of () is unit, Unit is equivalent to void in java but has one value denoted ()
+  val y = () // Unit = ()
   x * x * x * x
-
-  val result = 1.+(10)
+  // Value of block is the value of the last statement
+  val x0, y0, x1, y1 = 1
   result
 
   // ++, -- operators are not supported
@@ -43,26 +40,20 @@ object Sheet extends App {
 
   "Hello" (4) // Yields 'o'
   "Hello".apply(4) // Same as "Hello"(4)
-
-  var z = 1
-  // In Scala, an expression has a value
-  val exp = if (z > 0) 1 else -1 // Like x > 0 ? 1 : -1 in java
-  if (z > 0) "something" else -1 // Type is Any, Any = something
-
-  z = -1
-  if (z > 0) "something" // Any = ()
-
-  // Type of () is unit, Unit is equivalent to void in java but has one value denoted ()
-  val y = () // Unit = ()
-
-  // Value of block is the value of the last statement
-  val x0, y0, x1, y1 = 1
   val distance = {
     val dx = x0 - x1
     val dy = y0 - y1
     sqrt(dx * dx + dy * dy)
   }
+  // Var is mutable
+  var response = 54
+  if (z > 0) "something" else -1 // Type is Any, Any = something
 
+  z = -1
+  if (z > 0) "something" // Any = ()
+  var greeting: String = null
+  var count: Int = 2
+  var z = 1
   // If last expression is an assignment, the block value is ()
   var yy, i, n = 1
   while (yy > 0) {
@@ -95,18 +86,51 @@ object Sheet extends App {
   // Return type is inferred unsless the function is recursive in which cas explicit return type is required
   def fac(n: Int): Int = if (n <= 0) 1 else n * fac(n - 1)
 
+  // Or define return type Unit for Void method
+  def box2(s: String): Unit = {
+    box(s)
+  }
+
   // If we omit =, the function doesn't return a value. Like void funciton in java
   def box(s: String) {
     val border = "-" * s.length + "--\n"
     println(border + "|" + s + "|\n" + border)
   }
 
-  // Or define return type Unit for Void method
-  def box2(s: String): Unit = {
-    box(s)
-  }
-
   println(box("Mayank"))
   println(box2("Gupta"))
+
+  // Scala supports named arguments, is useful for greater clarity
+  // Scala also supports default arguments, lets to omit argument values
+  def decorate(s: String, left: String = "[", right: String = "]") = left + s + right
+
+  println(decorate("hello"))
+  println(decorate("hello", ">>["))
+  // Named argument
+  println(decorate("hello", right = "]<<"))
+  println(decorate("hello", ">>[", "]<<"))
+
+  // Variable number of arguments indicated by *, representing Seq[Int]
+  def sum(x: Int*) = {
+    var result = 0
+    for (i <- x) result += i
+    result
+  }
+
+  println(sum(1, 2, 3))
+  println(sum(1, 2, 3, 4, 5))
+
+  // if you already have Seq[Int], need declaration to pass it
+  // sum(1 to 10) // doesn't work
+  sum(1 to 10: _*) // works // _ tells scala to consider something as Seq of something
+
+  // declaration is also needed in recursive call
+  def recSum(args: Int*): Int = {
+    if (args.length == 0) 0
+    else args.head + recSum(args.tail: _*)
+  }
+
+  println(recSum(1, 2, 3))
+
 
 }
